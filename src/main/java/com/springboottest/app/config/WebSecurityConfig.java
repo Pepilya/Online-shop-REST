7a3b3f7a3a3d314f721e71cd8b6183d016239f07
Ilia <pepilya@yandex.ru>
2020-04-27 10:37:40 +0300

    add Spring Security JWT token without tests

--- src/main/java/com/springboottest/app/config/WebSecurityConfig.java
+++ src/main/java/com/springboottest/app/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.springboottest.app.config;
 import com.springboottest.app.service.CustomUserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
